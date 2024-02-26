fd83b8e8480ac5cf2517d09d904802bce711a66a
lmq8267 <119713693+lmq8267@users.noreply.github.com>
2024-02-26 23:26:37 +0800

    Update webd_status.sh

--- webd-SWRT/webd/scripts/webd_status.sh
+++ webd-SWRT/webd/scripts/webd_status.sh
@@ -5,9 +5,9 @@ webd_pid=`pidof webd`
 if [ -n "$webd_pid" ];then
         #webdver=`dbus get webd_version`
         #[ ! -z "$webdver" ] && webdver="webd $webdver "
-	webd_log="$webdver<span style='color:  #7FFF00'>运行中</span'><img src='https://www.right.com.cn/forum/data/attachment/album/202401/30/081238k459q2d5klacs8rk.gif' width='30px' alt=''> PID：$webd_pid"
+	webd_log="$webdver<span style='color:  #7FFF00'>运行中<img src='https://www.right.com.cn/forum/data/attachment/album/202401/30/081238k459q2d5klacs8rk.gif' width='30px' alt=''> PID：$webd_pid </span>"
 else
-	webd_log="<span style='color:  #FF0000'>未运行</span'>"
+	webd_log="<span style='color:  #FF0000'>未运行</span>"
 fi
 
 http_response "$webd_log"
